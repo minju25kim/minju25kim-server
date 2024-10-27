77263ae3551c5918e2baaf3ef7cb1e1b02a68150
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-10-28 02:27:45 +0900

    set vanilla extract

---
+++ src/style.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+
+export const buttonStyle = style({
+  backgroundColor: '#ccffee',
+  color: 'blue',
+  padding: '10px 20px',
+  borderRadius: '5px',
+  border: 'none',
+  cursor: 'pointer',
+});
