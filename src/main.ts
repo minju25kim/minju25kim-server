f001fd1db806735905c0f29a41316ee021c865b7
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-11-05 07:16:42 +0900

    feat: check en name

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { buttonStyle } from "./style.css.ts";
 import en from './resources/en.json'
 import kr from './resources/kr.json'
 
-console.log(en.basics.name)
 console.log(kr.basics.name)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
   <div>
+    <h1>${en.basics.name}</h1>
     <button id="myButton" class="${buttonStyle}">Download PDF</button>
   </div>
 `
