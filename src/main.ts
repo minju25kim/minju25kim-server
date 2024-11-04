ab25414123ae8a537461328627e94abc9a216ac9
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-11-05 07:18:47 +0900

    test cz

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { buttonStyle } from "./style.css.ts";
 import en from './resources/en.json'
 import kr from './resources/kr.json'
 
-console.log(kr.basics.name)
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
   <div>
     <h1>${en.basics.name}</h1>
+    <h1>${kr.basics.name}</h1>
     <button id="myButton" class="${buttonStyle}">Download PDF</button>
   </div>
 `
