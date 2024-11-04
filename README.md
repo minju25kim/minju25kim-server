63cfa7a74e3135474e01b1b3ed46a42f06d542fe
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-11-05 07:10:42 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# minju25kim-resume
+# resume
 
+## goal
+
+- [ ] i18n en/kr
+- [ ] download pdf
+- [ ] json to html
+- [ ] deploy aws free tier
+- [ ] dockerize.
+- [ ] simple dark/light theme
+- [ ] vanilla extract css
+
+
+## reference
+- rxresu web site
+- 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import './style.css'
 import { buttonStyle } from "./style.css.ts";
+import en from './resources/en.json'
+import kr from './resources/kr.json'
+
+console.log(en.basics.name)
+console.log(kr.basics.name)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
   <div>
-    <button id="myButton" class="${buttonStyle}">Click Me</button>
+    <button id="myButton" class="${buttonStyle}">Download PDF</button>
   </div>
 `
 
