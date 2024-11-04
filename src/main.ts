fd3ab9968b62f1b97eadbc16535f979d7311d756
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-11-05 07:43:22 +0900

    feat(main.ts): add lang button

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import kr from './resources/kr.json'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
   <div>
-    <h1>${en.basics.name}</h1>
-    <h1>${kr.basics.name}</h1>
+  <button id="myButton" class="${buttonStyle}">EN/한국어</button>
+    <div>
+    <h1>Resume</h1>
+    <h1>이력서</h1>
+    </div>
+    <h2>${en.basics.name}</h2>
+    <h2>${kr.basics.name}</h2>
+    <h2>${en.basics.location}</h2>
+    <h2>${kr.basics.location}</h2>
     <button id="myButton" class="${buttonStyle}">Download PDF</button>
   </div>
 `
